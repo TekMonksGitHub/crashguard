755bae9c90c287a07a86903f2b428ef98e94cffd
Rohit Kapoor <rvkapoor@tekmonks.com>
2020-04-07 06:17:25 -0400

    Allow spawning multiple processes via conf etc.

--- crashguard.js
+++ crashguard.js
@@ -1,13 +1,24 @@
+#!/usr/bin/env node
 /* 
  * crashguard.js, Runs local shell scripts or commands as servers
  * 
  * (C) 2020 TekMonks. All rights reserved.
  */
 const spawn = require("child_process").spawn;
-const conf = require(`${__dirname}/conf/process.json`);
+let conf; try{conf = require(`${__dirname}/conf/process.json`)}catch(err){}
 
-const callback = process=>spawnArgs(process,callback);
-spawnArgs(conf.process, callback);
+main();
+
+function main() {
+    const argv = [...process.argv].slice(2);
+    if (!conf) conf = argv.length?{process: argv}:undefined;
+
+    if (!conf) {console.log(`Usage: ${__filename} [process and arguments]`); process.exit(1);}
+    if (!Array.isArray(conf)) conf = [conf];
+
+    const callback = process=>spawnArgs(process, callback);
+    for (const process of conf) spawnArgs(process.process, callback);
+}
 
 function spawnArgs(args, callback) {
     const process = args.splice(0,1)[0]; 
@@ -17,7 +28,7 @@ function spawnArgs(args, callback) {
     shellProcess.stderr.on("data", data => console.error(data.toString("utf8")));
 
     shellProcess.on("exit", _ => {
-        console.error(`[CRASHGUARD] Process ${args} exited, restarting.`);
+        console.error(`[CRASHGUARD] Process ${[process,...args]} exited, restarting.`);
         callback([process,...args]);
     });
 }
