ca6912ba2acb3a827f53929305326f0f698f612d
Rohit Kapoor <rvkapoor@tekmonks.com>
2020-04-17 12:07:53 -0400

    Better to catch spawn on error.

--- crashguard.js
+++ crashguard.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-/*
+/* 
  * crashguard.js, Runs local shell scripts or commands as servers
- *
+ * 
  * (C) 2020 TekMonks. All rights reserved.
  */
 const spawn = require("child_process").spawn;
@@ -21,7 +21,7 @@ function main() {
 }
 
 function spawnArgs(args, callback) {
-    const processChild = args.splice(0,1)[0];
+    const processChild = args.splice(0,1)[0]; 
     const shellProcess = spawn(processChild, args);
     if (!shellProcess || !shellProcess.stdout || !shellProcess.stderr) {console.error(`Can't launch ${processChild} ${args}, exiting.`); process.exit(1);}
     shellProcess.stdout.on("data", data => console.log(data.toString("utf8")));
@@ -32,4 +32,6 @@ function spawnArgs(args, callback) {
         console.error(`[CRASHGUARD] Process ${[processChild,...args]} exited, restarting.`);
         callback([processChild,...args]);
     });
+
+    shellProcess.on("error", err => {console.error(err); console.error(`Can't launch ${processChild} ${args}, exiting.`); process.exit(1);});
 }
